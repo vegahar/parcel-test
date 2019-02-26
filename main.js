18ceaca6e5e4e0354e3c5f4d3652c3b7bb702f8a
Vegard Harper <vegard.harper@gmail.com>
2019-02-26 22:11:10 +0100

    changed to preact

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import { h, render, Component } from 'preact';
 
-class HelloMessage extends React.Component {
-    render () {
-        return <div>hello world</div>
+class Clock extends Component {
+    constructor() {
+        super();
+        // set initial time:
+        this.state.time = Date.now();
+    }
+
+    componentDidMount() {
+        // update time every second
+        this.timer = setInterval(() => {
+            this.setState({ time: Date.now() });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        // stop when not renderable
+        clearInterval(this.timer);
+    }
+
+    render(props, state) {
+        let time = new Date(state.time).toLocaleTimeString();
+        return <span>{ time }</span>;
     }
 }
+
+// render an instance of Clock into <body>:
+render(<Clock />, document.body);
